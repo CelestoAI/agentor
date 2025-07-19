3c976a1e837e4e86996ee213d4afd4d8610f5189
Aniket Maurya <theaniketmaurya@gmail.com>
2025-07-20 04:30:37 +0530

    Update license year and owner, add XML conversion utility for Pydantic models, and include corresponding tests. Update dependencies in pyproject.toml for development and new features.

--- src/zerozen/__init__.py
+++ src/zerozen/__init__.py
@@ -1,4 +1,5 @@
 from .cli import app
+from .output_text_formatter import pydantic_to_xml
 from .proxy import create_proxy
 
-__all__ = ["app", "create_proxy"]
+__all__ = ["app", "create_proxy", "pydantic_to_xml"]
