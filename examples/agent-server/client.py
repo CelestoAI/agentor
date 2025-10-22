3042ce0cba2494b5ab7d4cf83ba2b1a775443384
Aniket Maurya <theaniketmaurya@gmail.com>
2025-10-22 12:22:07 +0100

    simpler example

--- examples/agent-server/client.py
+++ examples/agent-server/client.py
@@ -2,8 +2,8 @@ import os
 import requests
 
 # When deployed to Celesto (agentor deploy --folder ./examples/agent-server)
-# URL = "https://api.celesto.ai/v1/deploy/apps/[APP_ID]/chat"
-URL = "http://localhost:8000/chat"
+URL = "https://api.celesto.ai/v1/deploy/apps/d2cdaacb-ee7e-424c-a7e5-42fc260a91d6/chat"
+# URL = "http://localhost:8000/chat"
 CELESTO_API_KEY = os.environ.get("CELESTO_API_KEY")
 
 headers = {
@@ -14,9 +14,6 @@ headers = {
 response = requests.post(
     URL,
     json={"input": "What is the weather in London?"},
-    stream=True,
     headers=headers,
 )
-for line in response.iter_lines():
-    if line:
-        print(line.decode("utf-8"), flush=True)
+print(response.content)
