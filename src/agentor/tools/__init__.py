561aad3c01752273f5889fd056414c097732bbdc
Aniket Maurya <theaniketmaurya@gmail.com>
2025-11-21 13:20:14 +0000

    feat: Add Calculator and Timezone tools and integrate them into the main tools module.

--- src/agentor/tools/__init__.py
+++ src/agentor/tools/__init__.py
@@ -1,20 +1,24 @@
 from .base import BaseTool, capability
+from .calculator import CalculatorTool
 from .exa import ExaSearchTool
 from .fetch import FetchTool
 from .git import GitTool
 from .github import GitHubTool
 from .postgres import PostgreSQLTool
 from .slack import SlackTool
+from .timezone import TimezoneTool
 from .weather import WeatherAPI
 
 __all__ = [
     "BaseTool",
     "capability",
+    "CalculatorTool",
     "ExaSearchTool",
     "FetchTool",
     "GitTool",
     "GitHubTool",
     "PostgreSQLTool",
     "SlackTool",
+    "TimezoneTool",
     "WeatherAPI",
 ]
