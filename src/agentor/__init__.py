aaa5de3ebb74411193396cb1d013c8c4ef947fcd
Aniket Maurya <theaniketmaurya@gmail.com>
2025-12-06 22:35:48 +0000

    Bump version from 0.0.17.dev0 to 0.0.17.dev1 (#99)

--- src/agentor/__init__.py
+++ src/agentor/__init__.py
@@ -13,7 +13,7 @@ from .utils import AppContext
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-__version__ = "0.0.17.dev0"
+__version__ = "0.0.17.dev1"
 
 __all__ = [
     "Agentor",
