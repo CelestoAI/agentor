32681fa516ae93452fe956292247fab365983f82
Aniket Maurya <theaniketmaurya@gmail.com>
2025-12-07 01:27:42 +0000

    fix

--- tests/perf/test_init_time.py
+++ tests/perf/test_init_time.py
@@ -2,15 +2,6 @@ import subprocess
 import time
 
 
-def test_import_time():
-    t0 = time.perf_counter()
-    assert subprocess.check_call(["uv", "run", "python", "-c", "import agentor"]) == 0
-    t1 = time.perf_counter()
-    assert t1 - t0 < 7, (
-        f"Package import should take less than 5 seconds but took {t1 - t0:.4f} seconds"
-    )
-
-
 def test_cli_time():
     t0 = time.perf_counter()
     assert subprocess.check_call(["celesto", "--help"]) == 0
