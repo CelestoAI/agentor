3d31a91f0a241993387c325cc4ed3a6c93674c4e
Aniket Maurya <theaniketmaurya@gmail.com>
2025-12-07 01:13:37 +0000

    update

--- tests/perf/test_init_time.py
+++ tests/perf/test_init_time.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 
 
@@ -7,6 +8,13 @@ def test_import_time():
     assert t1 - t0 < 5, (
         f"Package import should take less than 5 second but took {t1 - t0:.4f} seconds"
     )
+
+
+def test_cli_time():
+    t0 = time.perf_counter()
+    assert subprocess.check_call(["celesto", "--help"]) == 0
+    t1 = time.perf_counter()
+    assert t1 - t0 < 1, "CLI must be superfast"
 
 
 if __name__ == "__main__":
